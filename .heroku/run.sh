742dba34345197eb6fcbf5849440a5006d015e5b
Bartixxx32 <mcbplay1@gmail.com>
2019-07-12 09:22:51 +0200

    Update run.sh

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,1 +1,6 @@
-echo "test"
+wget -q https://github.com/pion/turn/releases/download/1.0.3/simple-turn-linux-amd64
+chmod +x simple-turn-linux-amd64
+export USERS='user=password foo=bar'
+export REALM=my-server.com
+export UDP_PORT=3478
+./simple-turn-linux-amd64
