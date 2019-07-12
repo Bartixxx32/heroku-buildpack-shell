e3c39ce359793b880ea33d88ff1c8c5055fef6b4
Bartixxx32 <mcbplay1@gmail.com>
2019-07-12 09:30:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
-fclose($myfile);
+touch("test.txt");
 ?>
